8b829ea58d6a14524316732ea2b7d6781bfb761e
Andre Ximenes <andreximenes536@gmail.com>
2026-01-21 09:32:32 -0300

    Configuração do projeto iniciados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://andreximenes.xyz",
+	output: "server",
+	build: {
+		inlineStylesheets: "always",
+	},
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	integrations: [preact()],
+	adapter: vercel(),
+});
